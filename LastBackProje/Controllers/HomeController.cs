b966d3a199d523cf563cfe7099c2d0c297cf607d
Amrahvn <amrahvn@code.edu.az>
2023-09-10 23:04:16 +0400

    added

---
+++ LastBackProje/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using LastBackProje.DataAccesLAyer;
+using LastBackProje.Models;
+using LastBackProje.ViewModels.HomeVms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LastBackProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            HomeVm homeVm = new HomeVm
+            {
+                //Sliders = await _context.Sliders.Where(s => s.IsDeleted == false).ToListAsync(),
+
+
+                categories= await _context.Categories.Where(c => c.IsDeleted == false && c.IsMain == true).ToListAsync(),
+
+                NewArrival = await _context.Products.Where(p=>p.IsDeleted == false && p.IsNewArrival).ToListAsync(),
+
+                BestSeller = await _context.Products.Where(p => p.IsDeleted == false && p.IsBestSeller).ToListAsync(),
+
+                Featured = await _context.Products.Where(p => p.IsDeleted == false && p.IsFeature).ToListAsync(),
+            };
+
+
+            return View(homeVm);
+        }
+
+       //public IActionResult SetCookie()
+       // {
+       //     Response.Cookies.Append("EmrahCookie", "Hello Guys");
+
+       //     return Ok();
+       // }
+
+       // public IActionResult GetCookie()
+       // {
+       //    string cookie=  Request.Cookies["basket"];
+
+       //     return Ok(cookie);
+       // }
+    }
+}
---
+++ LastBackProje/ViewComponents/tab-contentViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using LastBackProje.DataAccesLAyer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LastBackProje.ViewComponents
+{
+    public class tab_contentViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public tab_contentViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var tab=await _context.Products
+                .Where
+
+            //var sliders = await _context.Sliders
+            //   .Where(s => s.IsDeleted == false)
+            //   .ToListAsync();
+
+            //return View(sliders);
+        }
+    }
+}
